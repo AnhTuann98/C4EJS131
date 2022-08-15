7f956bffefb6b6b07881a246165cc75f0bcc41eb
unknown <duongvoanhtuan2015@gmail.com>
2022-08-15 12:32:23 +0700

    lab

---
+++ Lab_Buoi7/index.js
@@ -0,0 +1,34 @@
+// let username = 'Anh Tuan';
+// let email = 'duongvoanhtuan2015@gmail.com';
+// let age = 24;
+// console.log('username:', username);
+// console.log('age:',age);
+// console.log('email:',email);
+
+// //string
+let username = 'Anh Tuan';
+let email = 'duongvoanhtuan2015@gmail.com';
+
+// Number
+let a = 1;
+let b = 2; 
+let c = a + b;
+//console.log('a + b =', c);
+
+//Boolean True - False
+let isHiddenMenu = true;
+
+//Undefined
+let d;
+//console.log('d', d)
+
+//Null
+let e = null;
+//console.log('null', e)
+
+let greeting = 'Hello. My name is John';
+console.log
+
+
+
+
